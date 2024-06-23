aaf1be2287848afafe81c0a8823ed208e8c33401
BlockListed <44610569+BlockListed@users.noreply.github.com>
2024-06-23 14:50:41 +0200

    done for now

--- src/parse.rs
+++ src/parse.rs
@@ -13,16 +13,16 @@ use nom::sequence::preceded;
 use nom::sequence::separated_pair;
 use nom::sequence::terminated;
 use nom::sequence::tuple;
-use nom::IResult;
 use nom::AsChar;
+use nom::IResult;
 use nom::Parser;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Value {
     Number(i64),
     /// This also includes the original formatting of the float, so that we change the file as
-    /// little as possible.
-    Float(f64, Option<String>),
+    /// little as possible. You don't need to worry about that though.
+    Float(Float),
     Boolean(bool),
     Tuple(Vec<Value>),
     String(String),
@@ -32,83 +32,130 @@ pub enum Value {
     Null,
 }
 
+#[derive(Debug, Clone)]
+/// This also includes the original formatting.
+/// If modified, this will automatically serialize the new value, instead of the saved original.
+/// Basically just treat this as a float.
+pub struct Float(pub f64, pub(crate) String);
+
+impl Float {
+    pub fn new(v: f64) -> Float {
+        Float(v, String::new())
+    }
+}
+
+impl PartialEq for Float {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.eq(&other.0)
+    }
+}
+
 pub fn parse(vts: &str) -> Value {
     let (title, object) = parse_object(vts).unwrap().1;
-    
+
     Value::Object(vec![(title.to_string(), object)])
 }
 
 fn parse_object(vts: &str) -> IResult<&str, (&str, Value)> {
-    let (vts, title) = terminated(take_while1(|c: char| c.is_alpha() || c == '_'), multispace1)(vts)?;
+    let (vts, title) =
+        terminated(take_while1(|c: char| c.is_alpha() || c == '_'), multispace1)(vts)?;
 
-    let fields_parser = many1(delimited(space0, parse_object_field.map(|(t, v)| (t.to_owned(), v)), newline));
+    let fields_parser = many1(delimited(
+        space0,
+        parse_object_field.map(|(t, v)| (t.to_owned(), v)),
+        newline,
+    ));
 
-    let (vts, fields) = delimited(terminated(tag("{"), multispace0), fields_parser, preceded(multispace0, tag("}")))(vts)?;
+    let (vts, fields) = delimited(
+        terminated(tag("{"), multispace0),
+        fields_parser,
+        preceded(multispace0, tag("}")),
+    )(vts)?;
 
     Ok((vts, (title, Value::Object(fields))))
 }
 
 fn parse_array(vts: &str) -> nom::IResult<&str, (&str, Value)> {
-    let (vts, title) = terminated(take_while1(|c: char| c.is_alpha() || c == '_'), multispace1)(vts)?;
+    let (vts, title) =
+        terminated(take_while1(|c: char| c.is_alpha() || c == '_'), multispace1)(vts)?;
 
-    let element_parser = many0(terminated(parse_object.map(|(t, v)| (t.to_owned(), v)), multispace1));
+    let element_parser = many0(terminated(
+        parse_object.map(|(t, v)| (t.to_owned(), v)),
+        multispace1,
+    ));
 
-    let (vts, elements) = delimited(terminated(tag("{"), multispace0), element_parser, preceded(multispace0, tag("}")))(vts)?;
+    let (vts, elements) = delimited(
+        terminated(tag("{"), multispace0),
+        element_parser,
+        preceded(multispace0, tag("}")),
+    )(vts)?;
 
     Ok((vts, (title, Value::Array(elements))))
 }
 
 fn parse_value(vts: &str) -> IResult<&str, Value> {
-    eprintln!("parsing: {vts}");
     // number first
-    Ok(vts.parse::<i64>()
+    Ok(vts
+        .parse::<i64>()
         .ok()
-        .map(|v| Value::Number(v))
+        .map(Value::Number)
         // float
-        .or_else(|| vts.parse::<f64>().map(|v| Value::Float(v, Some(vts.to_owned()))).ok())
+        .or_else(|| {
+            vts.parse::<f64>()
+                .map(|v| Value::Float(Float(v, vts.to_owned())))
+                .ok()
+        })
         // boolean
-        .or_else(|| match vts {
+        .or(match vts {
             "True" => Some(Value::Boolean(true)),
             "False" => Some(Value::Boolean(false)),
             _ => None,
         })
         .map(|v| ("", v))
         // tuple
+        .or_else(|| parse_tuple(vts).ok())
         .or_else(|| {
-            parse_tuple(vts).ok()
-        })
-        .or_else(|| {
-            if vts == "" {
+            if vts.is_empty() {
                 return Some(("", Value::Null));
             }
 
             None
         })
         // string
-        .unwrap_or_else(|| ("", Value::String(vts.to_owned())))
-    )
+        .unwrap_or_else(|| ("", Value::String(vts.to_owned()))))
 }
 
 fn parse_tuple(vts: &str) -> IResult<&str, Value> {
-    let tuple_elems = many1(terminated(take_until1(",").and_then(parse_value), tuple((tag(","), space0)))).and(take_until1(")").and_then(parse_value));
+    let tuple_elems = many1(terminated(
+        take_until1(",").and_then(parse_value),
+        tuple((tag(","), space0)),
+    ))
+    .and(take_until1(")").and_then(parse_value));
 
-    delimited(terminated(tag("("), space0), tuple_elems, tag(")"))(vts).map(|(vts, (mut before, rest))| {
-        before.push(rest);
-        (vts, Value::Tuple(before))
-    })
+    delimited(terminated(tag("("), space0), tuple_elems, tag(")"))(vts).map(
+        |(vts, (mut before, rest))| {
+            before.push(rest);
+            (vts, Value::Tuple(before))
+        },
+    )
 }
 
 fn parse_object_field(vts: &str) -> IResult<&str, (&str, Value)> {
-    let parse_field = separated_pair::<_, _, _, _, nom::error::Error<&str>, _, _, _>(take_while1(|c: char| c.is_alpha() || c == '_'), tuple((space0, tag("="), space0)), take_until("\n").and_then(parse_value));
+    let parse_field = separated_pair::<_, _, _, _, nom::error::Error<&str>, _, _, _>(
+        take_while1(|c: char| c.is_alpha() || c == '_'),
+        tuple((space0, tag("="), space0)),
+        take_until("\n").and_then(parse_value),
+    );
 
     parse_field.or(parse_array).or(parse_object).parse(vts)
 }
 
 #[cfg(test)]
 mod testing {
-    use super::Value;
     use super::parse;
     use super::parse_tuple;
+    use super::Float;
+    use super::Value;
 
     const TEST_STR: &str = include_str!("../amogus testing.vts");
 
@@ -119,6 +166,16 @@ mod testing {
 
     #[test]
     fn test_tuple() {
-        assert_eq!(parse_tuple("(-234.3, 5, 403.3)"), Ok(("", Value::Tuple(vec![Value::Float(-234.3, Some("-234.3".into())), Value::Number(5), Value::Float(403.3, Some("403.3".into()))]))))
+        assert_eq!(
+            parse_tuple("(-234.3, 5, 403.3)"),
+            Ok((
+                "",
+                Value::Tuple(vec![
+                    Value::Float(Float(-234.3, "-234.3".into())),
+                    Value::Number(5),
+                    Value::Float(Float(403.3, "403.3".into()))
+                ])
+            ))
+        )
     }
 }
--- src/unparse.rs
+++ src/unparse.rs
@@ -1,6 +1,6 @@
 use std::iter::repeat;
 
-use crate::Value;
+use crate::{parse::Float, Value};
 
 pub fn unparse(v: &Value) -> String {
     assert!(matches!(v, Value::Object(_)), "top level should be object");
@@ -8,16 +8,19 @@ pub fn unparse(v: &Value) -> String {
         Value::Object(v) => v,
         _ => unreachable!(),
     };
-    
+
     let mut s = String::new();
 
     for (k, v) in v {
-        assert!(matches!(v, Value::Object(_)), "top level children should be objects");
+        assert!(
+            matches!(v, Value::Object(_)),
+            "top level children should be objects"
+        );
         let v = match v {
             Value::Object(v) => v,
             _ => unreachable!(),
         };
-        
+
         s.push_str(&unparse_object(0, k, v));
     }
 
@@ -32,13 +35,26 @@ fn indent(depth: u32) -> impl Iterator<Item = char> {
 fn unparse_value(v: &Value) -> Option<String> {
     match v {
         Value::Number(v) => Some(v.to_string()),
-        Value::Float(v, original) => Some(if let Some(o) = original { o.clone() } else { v.to_string() }),
-        Value::Boolean(v) => Some(if *v { "True"} else {"False"}.to_owned()),
+        Value::Float(v) => Some(unparse_float(v)),
+        Value::Boolean(v) => Some(if *v { "True" } else { "False" }.to_owned()),
         Value::Tuple(v) => Some(unparse_tuple(v)),
         Value::Null => Some(String::new()),
         Value::String(v) => Some(v.to_owned()),
         _ => None,
     }
+}
+
+fn unparse_float(v: &Float) -> String {
+    let Float(v, original) = v;
+    if original
+        .parse::<f64>()
+        .ok()
+        .is_some_and(|parsed| parsed.eq(v))
+    {
+        return original.clone();
+    }
+
+    v.to_string()
 }
 
 fn unparse_object(indent_depth: u32, k: &str, v: &[(String, Value)]) -> String {
@@ -51,8 +67,8 @@ fn unparse_object(indent_depth: u32, k: &str, v: &[(String, Value)]) -> String {
     s.push('{');
     s.push('\n');
     for (k, v) in v.iter() {
-        if let Some(v) = unparse_value(v) { 
-            s.extend(indent(indent_depth+1));
+        if let Some(v) = unparse_value(v) {
+            s.extend(indent(indent_depth + 1));
 
             s.push_str(k);
             s.push(' ');
@@ -62,11 +78,10 @@ fn unparse_object(indent_depth: u32, k: &str, v: &[(String, Value)]) -> String {
             s.push('\n')
         } else {
             s.push_str(&match v {
-                Value::Object(v) => unparse_object(indent_depth+1, k, v),
-                Value::Array(v) => unparse_array(indent_depth+1, k, v),
+                Value::Object(v) => unparse_object(indent_depth + 1, k, v),
+                Value::Array(v) => unparse_array(indent_depth + 1, k, v),
                 _ => unreachable!(),
-            }
-            )
+            })
         }
     }
     s.extend(indent(indent_depth));
@@ -86,13 +101,11 @@ fn unparse_array(indent_depth: u32, k: &str, v: &[(String, Value)]) -> String {
     s.push('{');
     s.push('\n');
     for (k, v) in v {
-        s.push_str(&
-        match v {
-            Value::Object(v) => unparse_object(indent_depth+1, k, v),
-            Value::Array(v) => unparse_array(indent_depth+1, k, v),
+        s.push_str(&match v {
+            Value::Object(v) => unparse_object(indent_depth + 1, k, v),
+            Value::Array(v) => unparse_array(indent_depth + 1, k, v),
             _ => panic!("invalid data"),
-        }
-        )
+        })
     }
     s.extend(indent(indent_depth));
     s.push('}');
